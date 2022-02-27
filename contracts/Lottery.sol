9610fed2115b9cb5ace99886aee1dd0eeb740a8f
Nikhil8847 <dukeforever95@gmail.com>
2022-02-27 15:48:55 +0530

    Virtual Machine error remove
    The error was that I was not funding the contract with enough link tokens for requesting randomness using fulfillrandomness

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,9 +7,9 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
-    uint256 public usdEntryFee;
-    uint256 randomness;
     address payable public recentWinner;
+    uint256 public randomness;
+    uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
     enum LOTTERY_STATE {
         OPEN,
@@ -18,36 +18,40 @@ contract Lottery is VRFConsumerBase, Ownable {
     }
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
-    bytes32 public keyHash;
-    event ReqestedRandomness(bytes32 requestId);
+    bytes32 public keyhash;
+    event RequestedRandomness(bytes32 requestId);
 
-
+    // 0
+    // 1
+    // 2
 
     constructor(
         address _priceFeedAddress,
-        address _vrfcoordinator,
+        address _vrfCoordinator,
         address _link,
         uint256 _fee,
-        bytes32 _keyHash
-    ) public VRFConsumerBase(_vrfcoordinator, _link) {
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
         fee = _fee;
-        keyHash = _keyHash;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
-        // 50$ minimum entrancefee
+        // $50 minimum
         require(lottery_state == LOTTERY_STATE.OPEN);
-        require(msg.value >= getEntranceFee(), "Not Enough Ether!");
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
-        uint256 adjustedPrice = uint256(price) * 10**10;
-
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        // $50, $2,000 / ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
@@ -61,20 +65,19 @@ contract Lottery is VRFConsumerBase, Ownable {
     }
 
     function endLottery() public onlyOwner {
-        // lottery_state = LOTTERY_STATE.cALCULATING_WINNER;
         // uint256(
-        //     keccak256(
+        //     keccack256(
         //         abi.encodePacked(
-        //             none,
-        //             msg.sender,
-        //             block.difficulty,
-        //             block.timestamp
+        //             nonce, // nonce is preditable (aka, transaction number)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can actually be manipulated by the miners!
+        //             block.timestamp // timestamp is predictable
         //         )
         //     )
         // ) % players.length;
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        bytes32 requestId = requestRandomness(keyHash, fee);
-        emit ReqestedRandomness(requestId);
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
     function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
@@ -85,13 +88,11 @@ contract Lottery is VRFConsumerBase, Ownable {
             lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
             "You aren't there yet!"
         );
-        require(_randomness > 0, "Random-not-found!");
+        require(_randomness > 0, "random-not-found");
         uint256 indexOfWinner = _randomness % players.length;
-
         recentWinner = players[indexOfWinner];
         recentWinner.transfer(address(this).balance);
-
-        //Reset
+        // Reset
         players = new address payable[](0);
         lottery_state = LOTTERY_STATE.CLOSED;
         randomness = _randomness;
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,35 +1,32 @@
 from brownie import (
-    Contract,
     accounts,
-    Lottery,
-    config,
     network,
+    config,
     MockV3Aggregator,
     VRFCoordinatorMock,
     LinkToken,
+    Contract,
+    interface,
 )
 
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account(index=None, id=None):
-    # Three ways to get the account
-    # 1. Use the brownie local accounts
-    # 2. Use accounts.add("env")
-    # 3. Use accounts.load("id")
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
     if index:
         return accounts[index]
     if id:
-        accounts.load(id)
+        return accounts.load(id)
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
-
-    else:
-        return accounts.add(config["wallets"]["from_key"])
+    return accounts.add(config["wallets"]["from_key"])
 
 
 contract_to_mock = {
@@ -40,33 +37,32 @@ contract_to_mock = {
 
 
 def get_contract(contract_name):
-    """
-    This function will grab the contract addresses from the bronie
-    config if defined, otherwise, it wll deploy a mock version of that
-    contract, and return taht mock contract.
-
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that mock contract.
 
         Args:
-            contract_name(string)
+            contract_name (string)
 
-
-        Returns: a contract
+        Returns:
             brownie.network.contract.ProjectContract: The most recently deployed
             version of this contract.
-
     """
     contract_type = contract_to_mock[contract_name]
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         if len(contract_type) <= 0:
+            # MockV3Aggregator.length
             deploy_mocks()
         contract = contract_type[-1]
+        # MockV3Aggregator[-1]
     else:
         contract_address = config["networks"][network.show_active()][contract_name]
-
+        # address
+        # ABI
         contract = Contract.from_abi(
-            contract_type.name, contract_address, contract_type.abi
+            contract_type._name, contract_address, contract_type.abi
         )
-
+        # MockV3Aggregator.abi
     return contract
 
 
@@ -74,21 +70,21 @@ DECIMALS = 8
 INITIAL_VALUE = 200000000000
 
 
-def deploy_mocks(decimals=DECIMALS, intial_value=INITIAL_VALUE):
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     account = get_account()
-    MockV3Aggregator.deploy(decimals, intial_value, {"from": account})
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
     link_token = LinkToken.deploy({"from": account})
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
-    print("Deployed!!!")
+    print("Deployed!")
 
 
 def fund_with_link(
-    contract_address, account=None, link_token=None, amount=100000000000000000
-):
+    contract_address, account=None, link_token=None, amount=5000000000000000000
+):  # 0.1 LINK
     account = account if account else get_account()
     link_token = link_token if link_token else get_contract("link_token")
     tx = link_token.transfer(contract_address, amount, {"from": account})
-    # link_token_contract = Interfaces.LinkTokenInterface(link_token.address)
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
     # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
     tx.wait(1)
     print("Fund contract!")
